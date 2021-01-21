8c48ea5bfb782fc76ed93fec341445f788f30de1
Elliacoj <amaury.jocaille@hotmail.com>
2021-01-21 14:28:37 +0100

    Modif

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ let table = $('table');
 
 getPlaylistsOptions();
 
+/**
+ * Function for creat a window for add a playlist
+ */
 function windowPlaylist() {
     windows();
 
@@ -48,6 +51,10 @@ deletePlaylist.click(function () {
     supPlaylist();
 });
 
+deleteSong.click(function () {
+    supSong();
+})
+
 /**
  * Create a new playlist
  * @param name The playlist name.
@@ -76,19 +83,19 @@ function createPlaylist(name, type) {
  * @param playlist
  * @param artist
  * @param title
- * @param duration
+ * @param link
  * @param youtubeUrl
  */
-function createSong(playlist, artist, title, duration, youtubeUrl) {
+function createSong(playlist, artist, title, link, youtubeUrl) {
     let song = {
         artist: artist,
         title: title,
-        duration: duration,
+        link: link,
         youtubeUrl: youtubeUrl
     }
+    playlist.songs.push(song);
 
-    playlist = loadPlaylist(playlist);
-
+    saveSong(playlist);
 }
 
 /**
@@ -114,6 +121,21 @@ function playlistExists(name) {
 }
 
 /**
+ * Return true is song already exists
+ * @param songName
+ * @param playlist
+ * @returns boolean True if exists, false otherwise
+ */
+function songExists(songName, playlist) {
+    if(playlist.songs.length > 0) {
+        return playlist.songs.reduce((accu, song) =>{ return accu + (song.title === songName) ? 1 : 0;}, 0) > 0
+    }
+    else {
+        return false
+    }
+}
+
+/**
  * Return the asked playlist.
  * @param name
  */
@@ -131,6 +153,9 @@ function getPlaylists() {
     return JSON.parse(localStorage.getItem('playlists')) ?? [];
 }
 
+/**
+ * Function for delete a playlist
+ */
 function supPlaylist() {
     windows();
 
@@ -169,8 +194,129 @@ function supPlaylist() {
     });
 }
 
+/**
+ * Function for delete a song
+ */
+function supSong() {
+    windows();
 
+    $('#window').append('<h2>Supprimer playlist</h2>',
+        '<label for="newName">Titre de la musique: </label>', '<input type="text" id="newName"><br>',
+        '<button id="confirm">Confirmer</button>', '<button id="cancel">Annuler</button>');
+
+    $('#cancel').click(function () {
+        $('#window').remove();
+        table.css('display', 'table');
+    });
+
+    $('#confirm').click(function () {
+        let name = $('#newName').val();
+        let check = $('#listPlaylist option:selected').attr('id');
+        let playlist = loadPlaylist(check);
+        let songs = playlist.songs;
+
+        if(songExists(name, playlist)) {
+            for(let song of songs) {
+                if(song.title === name) {
+                    let index = songs.indexOf(song);
+                    songs.splice(index, 1);
+
+                    saveSong(playlist);
+                }
+            }
+
+            $('#' + name + '').remove();
+            $('#window').remove();
+            table.css('display', 'table');
+        }
+        else {
+            alert();
+        }
+
+    });
+}
+
+/**
+ * Function for add a window empty
+ */
 function windows() {
     table.css('display', 'none');
     $('section').append('<div id="window" class="window"></div>');
 }
+
+/**
+ * Function for creat a window for creat and save a new song
+ */
+function newSongs() {
+    windows();
+
+    $('#window').append('<h2>Nouvelle musique</h2>',
+        '<label for="newArtiste">Artiste: </label>', '<input type="text" id="newArtiste"><br>',
+        '<label for="newTitle">Titre: </label>', '<input type="text" id="newTitle"><br>',
+        '<label for="newLink">Lien de la musique: </label>', '<input type="text" id="newLink"><br>',
+        '<label for="newYoutube">Lien Youtube: </label>', '<input type="text" id="newYoutube"><br>',
+        '<button id="confirm">Confirmer</button>', '<button id="cancel">Annuler</button>');
+
+    $('#cancel').click(function () {
+        $('#window').remove();
+        table.css('display', 'table');
+    });
+
+    $('#confirm').click(function () {
+        let artiste = $('#newArtiste').val();
+        let title = $('#newTitle').val();
+        let link = $('#newLink').val();
+        let youtube = $('#newYoutube').val();
+        let check = $('#listPlaylist option:selected').attr('id');
+
+        if($('#listPlaylist #listOfPlaylist').attr('id') !== check) {
+            let playlist = loadPlaylist(check);
+
+            if(!songExists(title, playlist)) {
+                createSong(playlist, artiste, title, link, youtube);
+                $('#window').remove();
+                table.css('display', 'table');
+                creatTableLine(artiste,title,youtube);
+            }
+            else {
+                alert();
+            }
+        }
+        else {
+            alert();
+        }
+    });
+}
+
+newSong.click(function () {
+    newSongs();
+})
+
+/**
+ * Function for creat a new line for the new song in the visual playlist
+ * @param artiste
+ * @param title
+ * @param youtube
+ */
+function creatTableLine(artiste, title, youtube) {
+    $('table tbody').append('<tr id="' + title + '"><td>' + title+ '</td><td>' + artiste + '</td><td></td><td>' + youtube + '</td></tr>');
+}
+
+/**
+ * Function for save changes of playlist in the localStorage
+ * @param playlistSave
+ */
+function saveSong(playlistSave) {
+    let playlists = getPlaylists()
+
+    for(let playlist of playlists) {
+        if(playlist.name === playlistSave.name) {
+            let index = playlists.indexOf(playlist);
+            playlists.splice(index, 1, playlistSave);
+        }
+    }
+
+    localStorage.removeItem('playlists');
+    localStorage.setItem('playlists', JSON.stringify(playlists));
+}
+
